7d0af21aacdcde23d6ab26384ff8968a64a63e94
higoreduardodocs <higoreduardodocs@gmail.com>
2023-10-16 09:23:05 -0400

    :lipstick: Refactor: Added list search list user

--- chat/src/app/components/search.jsx
+++ chat/src/app/components/search.jsx
@@ -4,6 +4,10 @@ export default function Search() {
       <div className="search-form">
         <input type="text" placeholder="Pesquisar" />
       </div>
+      <div className="user-chats">
+        <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg" />
+        <span>John Doe</span>
+      </div>
     </div>
   )
 }
