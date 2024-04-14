238ee7f013f71938d831b7abce6d380978975f25
higoreduardodocs <higoreduardodocs@gmail.com>
2024-04-14 14:58:55 -0400

    :art: Feat: create alias import

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
 import react from '@vitejs/plugin-react'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, 'src'),
+      },
+    ],
+  },
+  server: { port: 3000 },
 })
