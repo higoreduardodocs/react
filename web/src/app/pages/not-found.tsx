69b09d909a0b6abcfb06bd15282f914e3792054c
higoreduardodocs <higoreduardodocs@gmail.com>
2024-04-14 17:38:50 -0400

    :sparkles: Feat: create not found page

--- web/src/app/pages/not-found.tsx
+++ web/src/app/pages/not-found.tsx
@@ -1,3 +1,44 @@
+import { useNavigate } from 'react-router-dom'
+
+import { useWeekTrending } from '../../hooks/use-film'
+import { mergeFilms } from '../../utils/format'
+import Container from '../components/ui/container'
+import Heading from '../components/ui/heading'
+import Hero from '../components/ui/hero'
+import NoResults from '../components/ui/no-results'
+import Slider from '../components/ui/slider'
+
 export default function NotFound() {
-  return <section>Not found</section>
+  const navigate = useNavigate()
+  const trendings = mergeFilms(useWeekTrending('movie'), useWeekTrending('tv'))
+
+  return (
+    <>
+      <div className="relative h-[120px]">
+        <div className="overlay-film-cover" />
+        <div className="h-full w-full bg-primary" />
+      </div>
+      <Heading title="Página não encontrada" />
+      <Container>
+        <Slider className="slick-hero" autoplay={true}>
+          {(onSwipe: boolean) =>
+            !trendings?.length ? (
+              <NoResults />
+            ) : (
+              trendings?.map((item) => (
+                <Hero
+                  key={item.id}
+                  film={item}
+                  onClick={() => {
+                    !onSwipe
+                    navigate(`/${item.mediaType}/${item.id}`)
+                  }}
+                />
+              ))
+            )
+          }
+        </Slider>
+      </Container>
+    </>
+  )
 }
