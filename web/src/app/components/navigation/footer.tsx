fd0b04569c62b2202b1d5e4cf8a7052a698454f2
higoreduardodocs <higoreduardodocs@gmail.com>
2024-04-14 17:32:00 -0400

    :art: Refactor: create footer style

--- web/src/app/components/navigation/footer.tsx
+++ web/src/app/components/navigation/footer.tsx
@@ -1,3 +1,15 @@
+import Container from '../ui/container'
+
 export default function Footer() {
-  return <footer>Footer</footer>
+  return (
+    <Container>
+      <footer className="flex items-center justify-center flex-wrap w-full px-6 py-3 mx-auto border-t border-zinc-200">
+        <span>&copy; {new Date().getFullYear()}&nbsp;</span>
+        <span>Movie. Todos os direitos reservados. Feito com&nbsp;</span>
+        <span className="text-red-500">&#10084;</span>
+        <span>&nbsp;por&nbsp;</span>
+        <a className="font-semibold border-b border-white pb-1">Dudu</a>
+      </footer>
+    </Container>
+  )
 }
