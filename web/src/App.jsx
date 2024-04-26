d5e41fc5013baa5f1e18cfbefad5b853330fd0a0
higoreduardodocs <higoreduardodocs@gmail.com>
2024-04-25 22:43:41 -0400

    :pencil2: Fix: structure page

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Header from './components/header'
 import Home from './pages/home'
-import Category from './pages/home/category'
+import Category from './pages/category'
 
 export default function App() {
   return (
